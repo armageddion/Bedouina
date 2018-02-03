3496de4b8bb1223a97b829f315e112e81c3b1593
Armageddion <armageddion@gmail.com>
2018-02-02 23:49:11 -0500

    moar fix

--- setup/install.sh
+++ setup/install.sh
@@ -32,7 +32,7 @@ echo "[API KEY]" > $CONFIGFILE
 # prompt users for API keys
 echo "please enter your API key for dbip. go and get one from https://db-ip.com/api/free"
 read dbip
-if [[ $dbip == "" ]]; then
+if [ $dbip = "" ]; then
 	# user didn't provide an API key
 	echo "dbip: <n/a>" >> $CONFIGFILE
 else
@@ -41,7 +41,7 @@ fi
 
 echo "please enter your API key for openWeather. go and get one from https://home.openweathermap.org/users/sign_up"
 read openWeather
-if [[ $openWeather == "" ]]; then
+if [ $openWeather = "" ]; then
 	# user didn't provide an API key
 	echo "openWeather: <n/a>" >> $CONFIGFILE
 else
@@ -50,7 +50,7 @@ fi
 
 echo "please enter your API key for voicerss. go and get one from http://www.voicerss.org/registration.aspx"
 read voicerss
-if [[ $voicerss == "" ]]; then
+if [ $voicerss = "" ]; then
 	# user didn't provide an API key
 	echo "voicerss: <n/a>" >> $CONFIGFILE
 else
@@ -59,7 +59,7 @@ fi
 
 echo "please enter your API key for ifttt. go and get one from https://ifttt.com"
 read ifttt
-if [[ $ifttt == "" ]]; then
+if [ $ifttt = "" ]; then
 	# user didn't provide an API key
 	echo "ifttt_hook:: <n/a>" >> $CONFIGFILE
 else
@@ -68,7 +68,7 @@ fi
 
 echo "please enter your API key for pushbullet. go and get one from https://www.pushbullet.com/"
 read pushbullet
-if [[ $pushbullet == "" ]]; then
+if [ $pushbullet = "" ]; then
 	# user didn't provide an API key
 	echo "pushbullet: <n/a>" >> $CONFIGFILE
 else
@@ -80,13 +80,13 @@ echo "" >> $CONFIGFILE
 echo "[HUE dev]" >> $CONFIGFILE
 echo "if you have a Phillips Hue, please enter your Hue bridge MAC address"
 read huemac
-if [[ $huemac == "" ]]; then
+if [ $huemac = "" ]; then
 	# user didn't provide an API key
 	echo "<n/a>" >> $CONFIGFILE
 else
 	echo "please enter your hue developer token"
 	read huetoken
-	if [[ $huetoken == "" ]]; then
+	if [ $huetoken = "" ]; then
 		# user didn't provide an API key
 		echo "$huemac: <n/a>" >> $CONFIGFILE	
 	else
@@ -99,7 +99,7 @@ echo "" >> $CONFIGFILE
 echo "[Lifx]" >> $CONFIGFILE
 echo "if you have any Lifx, please enter your Lifx token"
 read lifx
-if [[ $lifx == "" ]]; then
+if [ $lifx = "" ]; then
 	# user didn't provide an API key
 	echo "token: <n/a>" >> $CONFIGFILE
 else
@@ -115,6 +115,7 @@ echo "still need to setup client_secret.json files for gmail and calendar"
 
 # set up the startup file
 echo "creating startup scripts"
+chmod a+x ../startup/b3na
 cp ../startup/b3na /etc/init.d/
 update-rc.d b3na defaults
 
