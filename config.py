05d1f1d274386aaaf316e919d006cafa5a0262a2
Armageddion <armageddion@gmail.com>
2018-11-20 12:57:23 -0500

    bump

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ config = ConfigParser.RawConfigParser()
 config.read(basedir,'../conf/apikeys.conf')
 
 class Config(object):
-	DATABASE_URL 	= os.environ.get('DATABASE_URL') or 'mysql://192.168.1.12'
+	DATABASE_URL 	= os.environ.get('DATABASE_URL') or 'mysql://192.168.1.19'
 	DATABASE_NAME 	= os.environ.get('DATABASE_NAME') or 'alfr3d'
 	DATABASE_USER 	= os.environ.get('DATABASE_USER') or 'alfr3d'
 	DATABASE_PSWD 	= os.environ.get('DATABASE_PSWD') or 'alfr3d'
