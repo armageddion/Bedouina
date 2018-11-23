22e7d2fc3e3a5e5720d62d1ec776c0ffecba368d
Armageddion <armageddion@gmail.com>
2018-11-23 12:59:19 -0500

    bump fixed

--- utilities/lights.py
+++ utilities/lights.py
@@ -81,8 +81,7 @@ def lightingOn(lights="all"):
 			print "switching device"+ device[0] + "on"
 			#TODO
 	else:
-		print "SELECT * FROM device WHERE device_type_id = "+str(dev_type)+" AND environment_id = "+str(env_id)+" AND name = "+lights+";" # DEBUG
-		cursor.execute("SELECT * FROM device WHERE device_type_id = "+str(dev_type)+" AND environment_id = "+str(env_id)+" AND name = "+lights+";")
+		cursor.execute("SELECT * FROM device WHERE device_type_id = "+str(dev_type)+" AND environment_id = "+str(env_id)+" AND name = \""+lights+"\"";")
 		device = cursor.fetchone()
 		print "switching device"+ device[0] + "on"
 		#TODO
@@ -110,7 +109,7 @@ def lightingOff(lights="all"):
 			print "switching device"+ device[0] + "off"
 			#TODO
 	else:
-		cursor.execute("SELECT * FROM device WHERE device_type_id = "+str(dev_type)+" AND environment_id = "+str(env_id)+" AND name = "+lights+";")
+		cursor.execute("SELECT * FROM device WHERE device_type_id = "+str(dev_type)+" AND environment_id = "+str(env_id)+" AND name = \""+lights+"\"";")
 		device = cursor.fetchone()
 		print "switching device"+ device[0] + "off"
 		#TODO
