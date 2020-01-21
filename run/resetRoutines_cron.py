aaef73a86237f5401b794c4044ed9410638a8855
Armageddion <armageddion@gmail.com>
2020-01-21 10:22:19 -0500

    temp cronjob to reset routines

---
+++ run/resetRoutines_cron.py
@@ -0,0 +1,7 @@
+import os
+import sys
+# import my own utilities
+sys.path.append(os.path.join(os.path.join(os.getcwd(),os.path.dirname(__file__)),"../"))
+import utilities
+
+utilities.resetRoutines()
